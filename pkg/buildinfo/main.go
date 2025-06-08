b6bd3705a9e82df0b0d1b4475c990870b654e16d
Timo Reymann <mail@timo-reymann.de>
2025-06-08 14:45:47 +0200

    chore(build): Add RFC1123 formatted build date to info

--- pkg/buildinfo/main.go
+++ pkg/buildinfo/main.go
@@ -13,10 +13,13 @@ var BuildTime = "2025-04-25_10:00:00"
 
 var BuildTimeParsed time.Time
 
+var BuildTimeRFC1123 string
+
 func init() {
 	parsed, err := time.Parse("2006-01-02_15:04:05", BuildTime)
 	if err != nil {
 		panic(err)
 	}
 	BuildTimeParsed = parsed
+	BuildTimeRFC1123 = parsed.Format(time.RFC1123)
 }
