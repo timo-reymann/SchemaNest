3da60d514559e90bd63971a2fff9562a481eed24
Timo Reymann <mail@timo-reymann.de>
2025-06-08 15:08:51 +0200

    chore(ui): Run eslint

--- pkg/ui/nextjs/config/site.ts
+++ pkg/ui/nextjs/config/site.ts
@@ -5,6 +5,6 @@ export const siteConfig = {
   description: "Where schemas grow, thrive, and scale with your team.",
   links: {
     github: "https://github.com/timo-reymann/SchemaNest",
-    apiDocs: "https://timo-reymann.github.io/SchemaNest/apidocs.html"
+    apiDocs: "https://timo-reymann.github.io/SchemaNest/apidocs.html",
   },
 };
