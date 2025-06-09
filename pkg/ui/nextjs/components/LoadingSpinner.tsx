3fe462deae7394ce85ca50bd39a9a18b29f41180
Timo Reymann <mail@timo-reymann.de>
2025-06-09 02:18:56 +0200

    chore(ui): Add reusable loading spinner

---
+++ pkg/ui/nextjs/components/LoadingSpinner.tsx
@@ -0,0 +1,14 @@
+import {Spinner} from "@heroui/spinner";
+import React from "react";
+
+export default function LoadingSpinner({label}: {label: string}) {
+    return <div className="flex h-full w-full">
+        <Spinner
+            className="justify-center mx-auto"
+            color="default"
+            label={label}
+            size="lg"
+            labelColor="foreground"
+        />
+    </div>
+}
