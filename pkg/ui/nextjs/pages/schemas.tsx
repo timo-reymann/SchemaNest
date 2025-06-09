d6441602d8c1d810fd2f98a8ee92d4e56624830f
Timo Reymann <mail@timo-reymann.de>
2025-06-09 17:15:04 +0200

    refactor(ui): Refactor overview page to use pages router and modular components

--- pkg/ui/nextjs/pages/schemas.tsx
+++ pkg/ui/nextjs/pages/schemas.tsx
@@ -3,25 +3,97 @@ import { Card, CardBody, CardFooter, CardHeader } from "@heroui/card";
 import { Code } from "@heroui/code";
 import { Pagination } from "@heroui/pagination";
 import React, { useEffect, useState } from "react";
-import NextLink from "next/link";
-import { useRouter } from "next/navigation";
+import { useRouter } from "next/router";
 import { Input } from "@heroui/input";
+import { Popover, PopoverContent, PopoverTrigger } from "@heroui/popover";
+import Link from "next/link";
 
 import { createHead } from "@/util/layoutHelpers";
 import { JsonSchemaInfo } from "@/store/jsonSchemas.slice";
 import { useBoundStore } from "@/store/main";
-import { JsonSchemaIcon, SearchIcon } from "@/components/icons";
+import { HelpIcon, JsonSchemaIcon, SearchIcon } from "@/components/icons";
 import LoadingSpinner from "@/components/LoadingSpinner";
 
 const MAX_PER_PAGE = 25;
 
+function SchemaCard({ schema }: { schema: JsonSchemaInfo }) {
+  const [showHelp, setShowHelp] = useState(false);
+
+  return (
+    <Card className="max-w[340px]">
+      <CardHeader className="justify-between">
+        <div className="flex gap-5">
+          <JsonSchemaIcon size={56} />
+          <div className="flex flex-col gap-1 items-start justify-center">
+            <h4 className="text-small font-semibold leading-none text-default-600">
+              {schema.identifier}
+            </h4>
+            <h5 className="text-small tracking-tight text-default-400">
+              v{schema.latestVersion.major}.{schema.latestVersion.minor}.
+              {schema.latestVersion.patch}
+            </h5>
+          </div>
+        </div>
+      </CardHeader>
+      <CardBody className="px-3 py-0 text-small text-default-400">
+        {schema.description ? (
+          <p>{schema.description}</p>
+        ) : (
+          <div className="flex gap-3">
+            <i>No description available</i>
+            <Popover
+              showArrow
+              classNames={{
+                base: ["before:bg-default-200"],
+                content: ["py-3 px-4 border border-default-200 max-w-80"],
+              }}
+              isOpen={showHelp}
+              placement="bottom-start"
+              onOpenChange={(open) => setShowHelp(open)}
+            >
+              <PopoverTrigger>
+                <HelpIcon
+                  onClick={(e) => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    setShowHelp(!showHelp);
+                  }}
+                  onMouseOut={() => setShowHelp(false)}
+                  onMouseOver={() => setShowHelp(true)}
+                />
+              </PopoverTrigger>
+              <PopoverContent>
+                <div className="px-1 py-2">
+                  <div className="font-bold">
+                    No Description Provided by Schema
+                  </div>
+                  <div className="text-small">
+                    The latest schema upload does not contain a{" "}
+                    <Code>description</Code> property.
+                  </div>
+                </div>
+              </PopoverContent>
+            </Popover>
+          </div>
+        )}
+      </CardBody>
+      <CardFooter className="flex justify-between">
+        <div className="flex gap-1">
+          <Code color="primary">JSON-Schema</Code>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
+
 export default function SchemasPage() {
   const router = useRouter();
+  const { page: pageQuery } = router.query as { page: string | undefined };
+
   const fetchSchemas = useBoundStore((s) => s.fetchSchemas);
   const filterAndPaginateSchemas = useBoundStore(
     (s) => s.filterAndPaginateSchemas,
   );
-  const schemas = useBoundStore((s) => s.schemas);
   const schemasLoading = useBoundStore((s) => s.schemaLoading);
 
   const [results, setResults] = useState<JsonSchemaInfo[]>([]);
@@ -31,20 +103,19 @@ export default function SchemasPage() {
   const [search, setSearch] = useState("");
 
   useEffect(() => {
-    const params = new URLSearchParams(window.location.search);
-    const page = params.get("page");
-
-    if (page) {
-      const parsedPag = Number.parseInt(page);
+    if (pageQuery) {
+      const parsedPag = Number.parseInt(pageQuery);
 
       setInitialPage(parsedPag);
       setPage(parsedPag);
     }
-  }, []);
+  }, [pageQuery]);
 
   useEffect(() => {
-    fetchSchemas();
-  }, [fetchSchemas]);
+    (async () => {
+      await fetchSchemas();
+    })();
+  }, []);
 
   useEffect(() => {
     let { slice: schemasToShow, totalPages } = filterAndPaginateSchemas(
@@ -58,20 +129,19 @@ export default function SchemasPage() {
   }, [schemasLoading, page, search]);
 
   const updatePage = (pageNum: number) => {
-    console.log("update page");
-    const params = new URLSearchParams(window.location.search);
+    const newQuery = { ...router.query, page: pageNum };
+    const url = {
+      pathname: router.pathname,
+      query: newQuery,
+    };
 
-    params.set("page", pageNum.toString());
-    const newUrl = `${window.location.pathname}?${params.toString()}`;
-
-    router.replace(newUrl, { scroll: false });
-
+    router.push(url, undefined, { shallow: true });
     setPage(pageNum);
   };
 
   const updateSearch = (search: string) => {
     setSearch(search);
-    setPage(1);
+    updatePage(1);
   };
 
   if (schemasLoading) {
@@ -96,40 +166,21 @@ export default function SchemasPage() {
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-4 p-4">
         {results.length === 0 && (
-          <>
-            <div className="col col-span-full text-center">No results</div>
-          </>
+          <div className="col col-span-full text-center">No results</div>
         )}
         {results.map((s, idx) => (
-          <NextLink
+          <Link
             key={idx}
-            href={`/schemas/json-schema/${s.identifier}/${s.latestVersion.major}.${s.latestVersion.minor}.${s.latestVersion.patch}`}
+            href={{
+              pathname: "/schemas/json-schema/[identifier]/[version]",
+              query: {
+                identifier: s.identifier,
+                version: `${s.latestVersion.major}.${s.latestVersion.minor}.${s.latestVersion.patch}`,
+              },
+            }}
           >
-            <Card className="max-w[340px]">
-              <CardHeader className="justify-between">
-                <div className="flex gap-5">
-                  <JsonSchemaIcon size={56} />
-                  <div className="flex flex-col gap-1 items-start justify-center">
-                    <h4 className="text-small font-semibold leading-none text-default-600">
-                      {s.identifier}
-                    </h4>
-                    <h5 className="text-small tracking-tight text-default-400">
-                      v{s.latestVersion.major}.{s.latestVersion.minor}.
-                      {s.latestVersion.patch}
-                    </h5>
-                  </div>
-                </div>
-              </CardHeader>
-              <CardBody className="px-3 py-0 text-small text-default-400">
-                <p>No description provided</p>
-              </CardBody>
-              <CardFooter className="flex justify-between">
-                <div className="flex gap-1">
-                  <Code color="primary">JSON-Schema</Code>
-                </div>
-              </CardFooter>
-            </Card>
-          </NextLink>
+            <SchemaCard schema={s} />
+          </Link>
         ))}
       </div>
 
