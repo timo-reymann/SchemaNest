6a1a77854c28afac26a644675d8065800a82406d
Timo Reymann <mail@timo-reymann.de>
2025-06-10 21:19:20 +0200

    chore(ui): Reformat app pages

--- ui/pages/_app.tsx
+++ ui/pages/_app.tsx
@@ -3,15 +3,20 @@ import { Viewport } from "next";
 import clsx from "clsx";
 import { AppProps } from "next/app";
 
-import { Providers } from "../styles/providers";
-
+import { Providers } from "@/styles/providers";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
 
 export const viewport: Viewport = {
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
+    {
+      media: "(prefers-color-scheme: light)",
+      color: "white",
+    },
+    {
+      media: "(prefers-color-scheme: dark)",
+      color: "black",
+    },
   ],
 };
 
@@ -26,7 +31,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
         <div className="relative flex flex-col h-screen">
           <Navbar />
-          <main className="container mx-auto pt-16 px-6 flex-grow">
+          <main className="container mx-auto pt-1 px-3 flex-grow">
             <Component {...pageProps} />
           </main>
         </div>
