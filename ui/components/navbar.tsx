434a3ebb3b399350a7665248f6070a09735cd34a
Timo Reymann <mail@timo-reymann.de>
2025-06-15 12:42:18 +0200

    fix(ui): Fix active link display

--- ui/components/navbar.tsx
+++ ui/components/navbar.tsx
@@ -1,70 +1,64 @@
-import {
-  Navbar as HeroUINavbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-} from "@heroui/navbar";
-import { Link } from "@heroui/link";
-import NextLink from "next/link";
-import clsx from "clsx";
-import { button as buttonStyles, link as linkStyles } from "@heroui/theme";
+import {Navbar as HeroUINavbar, NavbarBrand, NavbarContent, NavbarItem,} from "@heroui/navbar";
+import {Link} from "@heroui/link";
+import {button as buttonStyles} from "@heroui/theme";
 
-import { siteConfig } from "@/config/site";
-import { ThemeSwitch } from "@/components/theme-switch";
-import { GithubIcon, Logo } from "@/components/icons";
+import {siteConfig} from "@/config/site";
+import {ThemeSwitch} from "@/components/theme-switch";
+import {GithubIcon, Logo} from "@/components/icons";
+import {usePathname} from "next/navigation";
+import NextLink from "next/link";
 
 const navItems = [
-  {
-    label: "Overview",
-    href: "/schemas",
-  },
-  {
-    label: "API Documentation",
-    href: "/api-docs",
-  },
+    {
+        label: "Overview",
+        href: "/schemas",
+    },
+    {
+        label: "API Documentation",
+        href: "/api-docs",
+    },
 ];
 
 export const Navbar = () => {
-  return (
-    <HeroUINavbar maxWidth="2xl" position="static">
-      <NavbarContent className="basis-full" justify="start">
-        <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
-            <p className="font-bold text-inherit">SchemaNest</p>
-          </NextLink>
-        </NavbarBrand>
-        <ul className="lg:flex gap-4 justify-start ml-2">
-          {navItems.map((nav) => (
-            <NavbarItem key={nav.href}>
-              <NextLink
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-bold",
-                )}
-                color="foreground"
-                href={nav.href}
-              >
-                {nav.label}
-              </NextLink>
-            </NavbarItem>
-          ))}
-        </ul>
-      </NavbarContent>
+    const pathName = usePathname()
+    console.log(pathName)
+    return (
+        <HeroUINavbar maxWidth="2xl" position="static">
+            <NavbarContent className="basis-full" justify="start">
+                <NavbarBrand as="li" className="gap-3 max-w-fit">
+                    <NextLink className="flex justify-start items-center gap-1"
+                          color="foreground"
+                          href="/">
+                        <Logo/>
+                        <p className="font-bold text-inherit">SchemaNest</p>
+                    </NextLink>
+                </NavbarBrand>
+                <ul className="flex gap-4 justify-start ml-2">
+                    {navItems.map((nav) => (
+                        <NavbarItem key={nav.href}
+                                    isActive={nav.href === pathName}>
+                            <Link href={nav.href}
+                                  color={nav.href === pathName ? 'primary' : 'foreground'}>
+                                {nav.label}
+                            </Link>
+                        </NavbarItem>
+                    ))}
+                </ul>
+            </NavbarContent>
 
-      <NavbarContent className="sm:flex basis-1/5 sm:basis-full" justify="end">
-        <NavbarItem className="hidden sm:flex gap-2">
-          <ThemeSwitch />
-        </NavbarItem>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </NavbarContent>
-    </HeroUINavbar>
-  );
+            <NavbarContent className="sm:flex basis-1/5 sm:basis-full" justify="end">
+                <NavbarItem className="hidden sm:flex gap-2">
+                    <ThemeSwitch/>
+                </NavbarItem>
+                <Link
+                    isExternal
+                    className={buttonStyles({variant: "bordered", radius: "full"})}
+                    href={siteConfig.links.github}
+                >
+                    <GithubIcon size={20}/>
+                    GitHub
+                </Link>
+            </NavbarContent>
+        </HeroUINavbar>
+    );
 };
