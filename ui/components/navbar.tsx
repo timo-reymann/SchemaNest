4cad7a4495ca8835d5eb51932dd463c7e169e5e0
Timo Reymann <mail@timo-reymann.de>
2025-06-27 08:55:59 +0200

    fix(ui): Fix mobile navigation

--- ui/components/navbar.tsx
+++ ui/components/navbar.tsx
@@ -1,76 +1,98 @@
 import {
-  Navbar as HeroUINavbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
+    Navbar as HeroUINavbar,
+    NavbarBrand,
+    NavbarContent,
+    NavbarItem,
+    NavbarMenu,
+    NavbarMenuItem,
+    NavbarMenuToggle,
 } from "@heroui/navbar";
-import { Link } from "@heroui/link";
-import { button as buttonStyles } from "@heroui/theme";
-import { usePathname } from "next/navigation";
+import {Link} from "@heroui/link";
+import {button as buttonStyles} from "@heroui/theme";
+import {usePathname} from "next/navigation";
 import NextLink from "next/link";
 
-import { siteConfig } from "@/config/site";
-import { ThemeSwitch } from "@/components/theme-switch";
-import { GithubIcon, Logo } from "@/components/icons";
+import {siteConfig} from "@/config/site";
+import {ThemeSwitch} from "@/components/theme-switch";
+import {GithubIcon, Logo} from "@/components/icons";
+import {useState} from "react";
 
 const navItems = [
-  {
-    label: "Schemas",
-    href: "/schemas",
-  },
-  {
-    label: "Upload",
-    href: "/upload",
-  },
-  {
-    label: "API Documentation",
-    href: "/api-docs",
-  },
+    {
+        label: "Schemas",
+        href: "/schemas",
+    },
+    {
+        label: "Upload",
+        href: "/upload",
+    },
+    {
+        label: "API Documentation",
+        href: "/api-docs",
+    },
 ];
 
 export const Navbar = () => {
-  const pathName = usePathname();
+    const pathName = usePathname();
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
 
-  return (
-    <HeroUINavbar maxWidth="2xl" position="static">
-      <NavbarContent className="basis-full" justify="start">
-        <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink
-            className="flex justify-start items-center gap-1"
-            color="foreground"
-            href="/"
-          >
-            <Logo />
-            <p className="font-bold text-inherit">SchemaNest</p>
-          </NextLink>
-        </NavbarBrand>
-        <ul className="flex gap-4 justify-start ml-2">
-          {navItems.map((nav) => (
-            <NavbarItem key={nav.href} isActive={nav.href === pathName}>
-              <Link
-                color={nav.href === pathName ? "primary" : "foreground"}
-                href={nav.href}
-              >
-                {nav.label}
-              </Link>
-            </NavbarItem>
-          ))}
-        </ul>
-      </NavbarContent>
+    return (
+        <HeroUINavbar maxWidth="2xl" position="static" isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen}>
+            <NavbarContent className="basis-full" justify="start">
+                <NavbarBrand as="li" className="gap-3 max-w-fit">
+                    <NextLink
+                        className="flex justify-start items-center gap-1"
+                        color="foreground"
+                        href="/"
+                    >
+                        <Logo/>
+                        <p className="font-bold text-inherit">SchemaNest</p>
+                    </NextLink>
+                </NavbarBrand>
+                <ul className="flex gap-4 justify-start ml-2 hidden md:flex">
+                    {navItems.map((nav) => (
+                        <NavbarItem key={nav.href} isActive={nav.href === pathName}>
+                            <Link
+                                color={nav.href === pathName ? "primary" : "foreground"}
+                                href={nav.href}
+                            >
+                                {nav.label}
+                            </Link>
+                        </NavbarItem>
+                    ))}
+                </ul>
+            </NavbarContent>
 
-      <NavbarContent className="sm:flex basis-1/5 sm:basis-full" justify="end">
-        <NavbarItem className="hidden sm:flex gap-2">
-          <ThemeSwitch />
-        </NavbarItem>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </NavbarContent>
-    </HeroUINavbar>
-  );
+            <NavbarContent justify="end">
+                <NavbarItem>
+                    <ThemeSwitch/>
+                </NavbarItem>
+                <Link
+                    isExternal
+                    className={buttonStyles({variant: "bordered", radius: "full"})}
+                    href={siteConfig.links.github}
+                >
+                    <GithubIcon size={20}/>
+                    GitHub
+                </Link>
+            </NavbarContent>
+            <NavbarMenuToggle
+                aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+                className="sm:hidden"
+            />
+            <NavbarMenu>
+                {navItems.map((nav) => (
+                    <NavbarMenuItem key={nav.href}
+                                    isActive={nav.href === pathName}>
+                        <Link
+                            onPress={_ => setIsMenuOpen(false)}
+                            color={nav.href === pathName ? "primary" : "foreground"}
+                            href={nav.href}>
+                            {nav.label}
+                        </Link>
+                    </NavbarMenuItem>
+                ))}
+            </NavbarMenu>
+        </HeroUINavbar>
+    );
 };
--- ui/pages/_document.tsx
+++ ui/pages/_document.tsx
@@ -1,23 +1,24 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import {Head, Html, Main, NextScript} from "next/document";
 import clsx from "clsx";
 
-import { fontSans } from "@/config/fonts";
+import {fontSans} from "@/config/fonts";
 
 export default function Document() {
-  return (
-    <Html suppressHydrationWarning>
-      <Head>
-        <link href="/favicon.ico" rel="icon" />
-      </Head>
-      <body
-        className={clsx(
-          "min-h-screen text-foreground bg-background font-sans antialiased",
-          fontSans.variable,
-        )}
-      >
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html suppressHydrationWarning>
+            <Head>
+                <link href="/favicon.ico" rel="icon"/>
+                <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            </Head>
+            <body
+                className={clsx(
+                    "min-h-screen text-foreground bg-background font-sans antialiased",
+                    fontSans.variable,
+                )}
+            >
+            <Main/>
+            <NextScript/>
+            </body>
+        </Html>
+    );
 }
